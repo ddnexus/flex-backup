ecfc2f69d122bc06dd0756adf77cebc95f74eb09
Domizio Demichelis <dd.nexus@gmail.com>
2012-11-27 00:37:02 +0100

    tasks refactoring

--- lib/flex/backup.rb
+++ lib/flex/backup.rb
@@ -1,6 +1,15 @@
 module Flex
   module Backup
 
+    DEFAULT_OPTIONS = { :file       => './flex-backup.dump',
+                        :index      => nil,
+                        :type       => nil,
+                        :scroll     => '5m',
+                        :size       => 50,
+                        :timeout    => 20,
+                        :batch_size => 1000,
+                        :verbose    => true }
+
     extend self
 
     include Flex::Loader
@@ -10,22 +19,45 @@ module Flex
         match_all: {}
       yaml
 
+    def load_tasks
+      load File.expand_path('../../tasks.rb', __FILE__)
+    end
 
-    def dump_to_file(options)
-      vars       = { :index => options[:index],
-                     :type  => options[:type] }
-      total_hits = flex.count_search(:scan_all, vars)['hits']['total']
-      vars.merge :params => { :scroll => options[:scroll],
-                              :size   => options[:size],
-                              :fields => '_source,*' }
-      total_count = 0
-      pbar        = ProgBar.new(total_hits)
-      dump_stats  = Hash.new { |hash, key| hash[key] = Hash.new { |h, k| h[k] = 0 } }
-      file_size   = 0
-      File.open(options[:file], 'wb') do |file|
-        flex.scan_search(:scan_all, vars) do |result|
+    class Tasks
+
+      attr_reader :options
+
+      def initialize(overrides={})
+        options = Flex::Utils.env2options *Flex::Backup::DEFAULT_OPTIONS.keys
+
+        options[:size]       = options[:size].to_i       if options[:size]
+        options[:timeout]    = options[:timeout].to_i    if options[:timeout]
+        options[:batch_size] = options[:batch_size].to_i if options[:batch_size]
+
+        @options = DEFAULT_OPTIONS.merge(options).merge(overrides)
+      end
+
+
+      def dump_to_file
+        vars = { :index => options[:index],
+                 :type  => options[:type] }
+        if options[:verbose]
+          total_hits  = Backup.flex.count_search(:scan_all, vars)['hits']['total']
+          total_count = 0
+          pbar        = ProgBar.new(total_hits)
+          dump_stats  = Hash.new { |hash, key| hash[key] = Hash.new { |h, k| h[k] = 0 } }
+          file_size   = 0
+        end
+        vars.merge :params => { :scroll => options[:scroll],
+                                :size   => options[:size],
+                                :fields => '_source,*' }
+
+        file = options[:file].is_a?(String) ? File.open(options[:file], 'wb') : options[:file]
+        path = file.path
+
+        Backup.flex.scan_search(:scan_all, vars) do |result|
           lines = result['hits']['hits'].map do |h|
-                    dump_stats[h['_index']][h['_type']] += 1
+                    dump_stats[h['_index']][h['_type']] += 1 if options[:verbose]
                     meta = { :_index => h['_index'],
                              :_type  => h['_type'],
                              :_id    => h['_id'] }
@@ -34,52 +66,58 @@ module Flex
                         meta[k] = v if k[0] == '_'
                       end
                     end
-                    [MultiJson.encode({ 'index' => meta }),
-                     MultiJson.encode(h['_source'])].join("\n")
+                    [ MultiJson.encode({ 'index' => meta }),
+                      MultiJson.encode(h['_source']) ].join("\n")
                   end
           file.puts lines.join("\n")
-          total_count += lines.size
-          pbar.pbar.inc(lines.size)
+          if options[:verbose]
+            total_count += lines.size
+            pbar.pbar.inc(lines.size)
+          end
         end
-        file_size = file.size
-      end
-      formatted_file_size = file_size.to_s.reverse.gsub(/...(?=.)/, '\&,').reverse
-      pbar.pbar.finish
-      puts "\n***** WARNING: Expected document to dump: #{total_hits}, dumped: #{total_count}. *****" \
-           unless total_hits == total_count
-      puts "\nDumped #{total_count} documents to #{options[:file]} (size: #{formatted_file_size} bytes)"
-      puts dump_stats.to_yaml
-    end
+        file_size = file.size if options[:verbose]
+        file.close
 
-    def load_from_file(options)
-      Configuration.http_client_options[:timeout] = options[:timeout]
-      chunk_size = options[:batch_size] * 2 # 2 lines per doc
-      lines      = ''
-      line_count = 0
-      file       = File.open(options[:file])
-      file.lines { line_count += 1 }
-      file.rewind
-      pbar = ProgBar.new(line_count / 2, options[:batch_size])
-      file.lines do |line|
-        lines << line
-        if file.lineno % chunk_size == 0
+        if options[:verbose]
+          formatted_file_size = file_size.to_s.reverse.gsub(/...(?=.)/, '\&,').reverse
+          pbar.pbar.finish
+          puts "\n***** WARNING: Expected document to dump: #{total_hits}, dumped: #{total_count}. *****" \
+               unless total_hits == total_count
+          puts "\nDumped #{total_count} documents to #{path} (size: #{formatted_file_size} bytes)"
+          puts dump_stats.to_yaml
+        end
+      end
+
+      def load_from_file
+        Configuration.http_client_options[:timeout] = options[:timeout]
+        chunk_size = options[:batch_size] * 2 # 2 lines per doc
+        lines      = ''
+        file       = options[:file].is_a?(String) ? File.open(options[:file]) : options[:file]
+        path       = file.path
+        if options[:verbose]
+          line_count = 0
+          file.lines { line_count += 1 }
+          file.rewind
+          puts "\nLoading from #{path}...\n"
+          pbar = ProgBar.new(line_count / 2, options[:batch_size])
+        end
+        file.lines do |line|
+          lines << line
+          if file.lineno % chunk_size == 0
+            result = Flex.bulk :lines => lines
+            lines  = ''
+            pbar.process_result(result, options[:batch_size]) if options[:verbose]
+          end
+        end
+        # last chunk
+        unless lines == ''
           result = Flex.bulk :lines => lines
-          lines  = ''
-          pbar.process_result(result, options[:batch_size])
+          pbar.process_result(result, (file.lineno % chunk_size) / 2) if options[:verbose]
         end
+        file.close
+        pbar.finish if options[:verbose]
       end
-      # last chunk
-      unless lines == ''
-        result = Flex.bulk :lines => lines
-        pbar.process_result(result, (file.lineno % chunk_size) / 2)
-      end
-      file.close
-      pbar.finish
-    end
 
-    def load_tasks
-      load File.expand_path('../../tasks.rb', __FILE__)
     end
-
   end
 end
--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,23 +1,33 @@
-option_keys = [:index, :type, :scroll, :size, :file, :timeout, :batch_size]
-options     = { }
+require 'flex'
+require 'flex-backup'
 
-ENV.keys.map do |k|
-  key = k.downcase.to_sym
-  options[key] = ENV[k] if option_keys.include?(key)
-end
-
-env = defined?(Rails) ? :environment : []
+env   = defined?(Rails) ? :environment : []
+tasks = Flex::Backup::Tasks.new
 
 namespace :flex do
 
   desc 'Dumps the data from one or more ElasticSearch indices to a file'
-  task(:dump => env) do
-    Flex::Backup.dump_to_file(options)
-  end
+  task(:dump => env) { tasks.dump_to_file }
 
   desc 'Loads a dumpfile into ElasticSearch'
-  task(:load => env) do
-    Flex::Backup.load_from_file(options)
+  task(:load => env) { tasks.load_from_file }
+
+  desc 'dump, re-create the index and load the data again'
+  task(:reindex => env) do
+    require 'tempfile'
+    begin
+      tempfile = Tempfile.new %w[flex-backup dump], :encoding => 'UTF-8'
+      tasks.options[:file] = tempfile
+      tasks.dump_to_file
+
+      Flex::Tasks.new(:force => true).create_indices
+
+      tempfile.open
+      tasks.load_from_file
+    ensure
+      tempfile.close
+      tempfile.unlink
+    end
   end
 
 end

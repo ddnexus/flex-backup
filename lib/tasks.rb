f543b3116f981909ec3a3e3677309463f8a4b0c7
Domizio Demichelis <dd.nexus@gmail.com>
2013-05-22 09:20:19 +0200

    namespaced tasks

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -4,29 +4,31 @@ require 'flex-backup'
 env = defined?(Rails) ? :environment : []
 
 namespace :flex do
+  namespace :backup do
 
-  desc 'Dumps the data from one or more ElasticSearch indices to a file'
-  task(:dump => env) { Flex::Backup::Tasks.new.dump_to_file }
+    desc 'Dumps the data from one or more ElasticSearch indices to a file'
+    task(:dump => env) { Flex::Backup::Tasks.new.dump_to_file }
 
-  desc 'Loads a dumpfile into ElasticSearch'
-  task(:load => env) { Flex::Backup::Tasks.new.load_from_file }
+    desc 'Loads a dumpfile into ElasticSearch'
+    task(:load => env) { Flex::Backup::Tasks.new.load_from_file }
 
-  desc 'dump, re-create the index and load the data again'
-  task(:reindex => env) do
-    tasks = Flex::Backup::Tasks.new
-    require 'tempfile'
-    begin
-      tempfile = Tempfile.new %w[flex-backup dump], :encoding => 'UTF-8'
-      tasks.options[:file] = tempfile
-      tasks.dump_to_file
+    desc 'Dumps and re-creates the index by importing the data again'
+    task(:reindex => env) do
+      tasks = Flex::Backup::Tasks.new
+      require 'tempfile'
+      begin
+        tempfile = Tempfile.new %w[flex-backup dump], :encoding => 'UTF-8'
+        tasks.options[:file] = tempfile
+        tasks.dump_to_file
 
-      Flex::Tasks.new(:force => true).create_indices
+        Flex::Tasks.new(:force => true).create_indices
 
-      tempfile.open
-      tasks.load_from_file
-    ensure
-      tempfile.close
-      tempfile.unlink
+        tempfile.open
+        tasks.load_from_file
+      ensure
+        tempfile.close
+        tempfile.unlink
+      end
     end
   end
 
